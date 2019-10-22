a0acd9504895fb3bc0e1d8ed7572fb430d603244
Matheus Santos <matheus.santos@ilegra.com>
2019-10-22 10:17:02 -0300

    Adicionada aula sobre diretiva ng-if.
    Ajustados componentes em pasta específica.

--- Aulas Angular/my-app/src/app/app.module.ts
+++ Aulas Angular/my-app/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './components/employees-list/employee-list.component';
-import { EmployeeDetailComponent } from './components/employees-details/employee-detail.component';
 import { EmployeeService } from './services/employee.service';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
+import { NgModelComponent } from './components/ng-model/ng-model.component';
+import { EmployeeListComponent } from './components/ng-for/employees-list/employee-list.component';
+import { EmployeeDetailComponent } from './components/ng-for/employees-details/employee-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeeListComponent, EmployeeDetailComponent],
+  declarations: [
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent,
+    NgIfComponent,
+    NgModelComponent
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
---
+++ Aulas Angular/my-app/src/app/components/ng-model/ng-model.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-model',
+  templateUrl: './ng-model.component.html',
+  styleUrls: ['./ng-model.component.scss']
+})
+export class NgModelComponent implements OnInit {
+  
+  public name = 'Eduardo';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
