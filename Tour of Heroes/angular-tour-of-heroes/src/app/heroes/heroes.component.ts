e4523618879177fef59940518b217a1fc5d5ea12
Matheus Santos <matheus.santos@ilegra.com>
2019-10-28 15:29:46 -0300

    Adicionadas rotas e dashboard.

--- Tour of Heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Tour of Heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,23 +9,15 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
-
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
